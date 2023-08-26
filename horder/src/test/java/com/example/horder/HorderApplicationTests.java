0a6f66b1e4c677728d93731fd425e8900331f24d
tweener023 <patchedupsrb@gmail.com>
2023-08-26 11:41:20 +0200

    config

---
+++ horder/src/test/java/com/example/horder/HorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.horder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
